3e7c154d42c51286bb780b4686577f215fe58f3a
PereyraTL <thomaspereyra014@gmail.com>
2023-12-28 17:24:40 -0300

    add eslint and standard

--- projects/01-tic-tac-toe/src/App.jsx
+++ projects/01-tic-tac-toe/src/App.jsx
@@ -1,44 +1,42 @@
 import { useState } from 'react'
-import confetti from "canvas-confetti"
+import confetti from 'canvas-confetti'
 import { Square } from './components/Square.jsx'
-import {TURNS} from "./constants.js"
+import { TURNS } from './constants.js'
 import { checkWinnerFrom, checkEndGame } from './logic/board.js'
 import { WinnerModal } from './components/WinnerModal.jsx'
-import { saveGameToStorage, resetGameStorage} from './logic/storage/index.js'
+import { saveGameToStorage, resetGameStorage } from './logic/storage/index.js'
 
-//estado -> valor que cada vez que cambie va a renderizar de nuevo el comp
+// estado -> valor que cada vez que cambie va a renderizar de nuevo el comp
 // en square del map no se pasa la ejecucion, pq quiero q se ejecute cuando hago click, no cuando se renderiza
 // aprender dif entre spread y rest operator
 // siempre tratar a los estados como inmutables (problems de render)
-function App() {
-  //estados 
+function App () {
+  // estados
   const [board, setBoard] = useState(() => {
     const boardFromStorage = window.localStorage.getItem('board')
-    console.log('boardFromStorage:', boardFromStorage);
-    if(boardFromStorage) return JSON.parse(boardFromStorage)   
+    console.log('boardFromStorage:', boardFromStorage)
+    if (boardFromStorage) return JSON.parse(boardFromStorage)
     return Array(9).fill(null)
   })
-  const [turn,setTurn] = useState(() => {
+  const [turn, setTurn] = useState(() => {
     const turnFromStorage = window.localStorage.getItem('turn')
     return turnFromStorage ?? TURNS.X
   })
-  //null = no hay ganador, false = empate
-  const [winner, setWinner] = useState(null) 
+  // null = no hay ganador, false = empate
+  const [winner, setWinner] = useState(null)
 
   const resetGame = () => {
     setBoard(Array(9).fill(null))
     setTurn(TURNS.X)
     setWinner(null)
-    
+
     resetGameStorage()
   }
-
-  
 
   const updateBoard = (index) => {
     // no actualizamos esta posicion
     // si ya tiene algo
-    if(board[index] || winner) return
+    if (board[index] || winner) return
     // actualizar el tablero
     const newBoard = [...board]
     newBoard[index] = turn
@@ -47,11 +45,11 @@ function App() {
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
     // guardar partida aca por si acado
-    saveGameToStorage({board:newBoard, turn:newTurn})
+    saveGameToStorage({ board: newBoard, turn: newTurn })
     // revisar si hay ganador
     // la actualizacion del estado es asinc
     const newWinner = checkWinnerFrom(newBoard)
-    if(newWinner) {
+    if (newWinner) {
       confetti()
       setWinner(newWinner)
     } else if (checkEndGame(newBoard)) {
@@ -65,7 +63,7 @@ function App() {
       <button onClick={resetGame}>Reset del juego</button>
       <section className='game'>
         {
-          board.map((square,index) => {
+          board.map((square, index) => {
             return (
               <Square
                 key={index}
@@ -85,7 +83,7 @@ function App() {
       </section>
       <WinnerModal resetGame={resetGame} winner={winner} />
     </main>
-    
+
   )
 }
 
